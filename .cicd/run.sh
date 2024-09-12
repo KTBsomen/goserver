21879d3f05bffc58db2c686809f091dc56aa3c13
somen das <53004533+KTBsomen@users.noreply.github.com>
2024-09-12 18:22:41 +0530

    Update run.sh

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Kill any process running on port 8080
+lsof -i:8080 | awk 'NR!=1 {print $2}' | xargs -r kill -9
 /usr/local/go/bin/go mod init hello-world
 /usr/local/go/bin/go mod tidy
 /usr/local/go/bin/go run main.go 
