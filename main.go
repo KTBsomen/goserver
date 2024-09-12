315e625ddb21a1b5975a56d44972ef1d37e983fb
somen das <53004533+KTBsomen@users.noreply.github.com>
2024-09-12 18:55:59 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "new changed ,bjgfxfghhfHello, World!")
+    fmt.Fprintf(w, "new changed ,jjjjj, World!")
 }
 
 func main() {
