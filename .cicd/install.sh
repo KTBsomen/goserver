2763e8a891b32f35655424fb4e557fd6749a1a54
somen das <53004533+KTBsomen@users.noreply.github.com>
2024-09-12 15:21:50 +0530

    Create install.sh

---
+++ .cicd/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define Golang version
+GO_VERSION="1.21.1"  # Change this to the version you need
+
+# Download and install Go
+echo "Downloading Go $GO_VERSION..."
+wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
+
+echo "Removing existing Go installation..."
+sudo rm -rf /usr/local/go
+
+echo "Installing Go..."
+sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
+
+# Set up Go environment variables
+echo "Setting up Go environment variables..."
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
+# Verify Go installation
+echo "Verifying Go installation..."
+go version
+
+# Clean up
+rm "go$GO_VERSION.linux-amd64.tar.gz"
+
+echo "Go installation complete."
