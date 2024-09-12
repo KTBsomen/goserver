5332b2d86d3904f8375290ec0b2f944a21eb2c6e
somen das <53004533+KTBsomen@users.noreply.github.com>
2024-09-12 18:12:40 +0530

    Update install.sh

--- .cicd/install.sh
+++ .cicd/install.sh
@@ -6,20 +6,26 @@ read -s SUDO_PASSWORD
 # Define Golang version
 GO_VERSION="1.21.1"  # Change this to the version you need
 
+# Check if /usr/local/go exists
+if [ -d "/usr/local/go" ]; then
+    echo "Go is already installed in /usr/local/go. Exiting."
+    exit 0
+fi
+
 # Download and install Go
 echo "Downloading Go $GO_VERSION..."
 wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
-
-echo "Removing existing Go installation..."
-sudo rm -rf /usr/local/go
 
 echo "Installing Go..."
 sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
 
 # Set up Go environment variables
 echo "Setting up Go environment variables..."
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-source ~/.bashrc
+
+echo "Setting up Go environment variables..."
+echo "GOROOT=/usr/local/go" | sudo tee -a /etc/environment > /dev/null
+echo "PATH=$PATH:/usr/local/go/bin" | sudo tee -a /etc/environment > /dev/null
+
 
 # Verify Go installation
 echo "Verifying Go installation..."
@@ -29,3 +35,4 @@ go version
 rm "go$GO_VERSION.linux-amd64.tar.gz"
 
 echo "Go installation complete."
+#22
