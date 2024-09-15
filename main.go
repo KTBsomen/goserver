fb13375c8caebc08b2b655b65460719491e3c1ec
somen das <53004533+KTBsomen@users.noreply.github.com>
2024-09-16 02:29:33 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, " lets check againn tgggg") 
+    fmt.Fprintf(w, " lets check againn tg") 
     
 }
 
