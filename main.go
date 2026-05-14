104247eb2296c187e5b42f539406f6df54910922
somen das <53004533+KTBsomen@users.noreply.github.com>
2026-05-14 23:11:51 +0530

    s

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, " lets check newcicd jbj bbb") 
+    fmt.Fprintf(w, " lets check newcicd 1") 
     
     
 }
