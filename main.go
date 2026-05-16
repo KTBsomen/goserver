92ac12e080431a1b19e320378e4b989bdb2188a1
somen das <53004533+KTBsomen@users.noreply.github.com>
2026-05-16 14:34:36 +0530

    Update helloWorldHandler response message

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, " lets check rcommit message check single check") 
+    fmt.Fprintf(w, " lets check rcommit message check single check2") 
     
     
 }
